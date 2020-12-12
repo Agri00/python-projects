3a479011a080894cacde23786df14e5725913f03
Agri00 <68244961+Agri00@users.noreply.github.com>
2020-12-12 17:33:38 +0100

    Add files via upload

---
+++ web-scraper/file.py
@@ -0,0 +1,7 @@
+import scraper
+import sys
+
+scraped_info = open("scraped_info.txt", "a")
+scraped_info.write("Ahmed Negri")
+scraped_info.write("\nAhmed Negri est un mec vachement balasie")
+scraped_info.close()
